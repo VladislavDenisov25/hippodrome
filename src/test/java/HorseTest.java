a7d1e7bea4ddac5325ba17d743e74e42bf472a35
VladislavDenisov25 <vladislav-denisov@mail.ru>
2026-02-10 12:29:03 +0300

    создание теста для метода move() класса Horse

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -2,8 +2,11 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Answers.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mockStatic;
 
 
 public class HorseTest {
@@ -18,42 +21,58 @@ public class HorseTest {
 
     @ParameterizedTest
     @CsvSource({"' '", "'\n'", "'\t'", "''"})
-    public void constructorThrowsExceptionWhenNameIsBlank(String value){
+    public void constructorThrowsExceptionWhenNameIsBlank(String value) {
         Exception ex = assertThrows(IllegalArgumentException.class, () -> {
             new Horse(value, 3d, 3d);
         });
         assertEquals("Name cannot be blank.", ex.getMessage());
     }
+
     @Test
-    public void constructorThrowsExceptionWhenNegativeSpeed(){
-        Exception ex = assertThrows(IllegalArgumentException.class, () ->{
+    public void constructorThrowsExceptionWhenNegativeSpeed() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
             new Horse("Gass", -5d, 1d);
         });
         assertEquals("Speed cannot be negative.", ex.getMessage());
     }
+
     @Test
-    public void constructorThrowsExceptionWhenNegativeDistance(){
-        Exception ex = assertThrows(IllegalArgumentException.class, () ->{
+    public void constructorThrowsExceptionWhenNegativeDistance() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
             new Horse("Gass", 5d, -1d);
         });
         assertEquals("Distance cannot be negative.", ex.getMessage());
     }
+
     @Test
-    public void getNameReturnsCorrectName(){
+    public void getNameReturnsCorrectName() {
         Horse gass = new Horse("Gass", 5d, 5d);
         assertEquals("Gass", gass.getName());
     }
+
     @Test
-    public void getNameReturnsCorrectSpeed(){
+    public void getSpeedReturnsCorrectSpeed() {
         Horse gass = new Horse("Gass", 5d, 5d);
         assertEquals(5d, gass.getSpeed());
     }
+
     @Test
-    public void getNameReturnsCorrectDistance(){
+    public void getDistanceReturnsCorrectDistance() {
         Horse gass = new Horse("Gass", 5d, 5d);
         assertEquals(5d, gass.getDistance());
         Horse gass2 = new Horse("Gass", 5d);
         assertEquals(0d, gass2.getDistance());
+    }
 
+    @ParameterizedTest
+    @ValueSource(doubles = {0.5d})
+    public void getMoveReturnsCorrectMove(double random) {
+        Horse gass = new Horse("Gass", 5d, 5d);
+        try (MockedStatic<Horse> mocked = mockStatic(Horse.class, CALLS_REAL_METHODS)) {
+            mocked.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(random);
+            gass.move();
+            mocked.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+            assertEquals(5d + 5d * random, gass.getDistance());
+        }
     }
 }
