bbd045b041470b97b4ab42928ca38957e4d80d76
VladislavDenisov25 <vladislav-denisov@mail.ru>
2026-02-10 20:23:42 +0300

    добавление логера log4j2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -15,7 +19,7 @@ public class Main {
                 new Horse("Cherry", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
-
+        logger.info(String.format("Начало скачек. Количество участников: %d", horses.size()));
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
             watch(hippodrome);
@@ -24,6 +28,7 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
+        logger.info(String.format("Окончание скачек. Победитель: %s", winnerName));
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
